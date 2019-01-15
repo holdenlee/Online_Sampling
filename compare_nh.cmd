3281e1009191002edb77cd364df3e8dbef206766
Holden Lee <holdenl@princeton.edu>
2019-01-15 16:05:43 -0500

    1-15

--- compare_nh.cmd
+++ compare_nh.cmd
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python compare_nh.py 0.1 1000 ../outputs/compare_time_0_1_trials_1000/seed 100 > ../outputs/compare_time_0_1_trials_1000/log.log 2>&1 &
+nohup python src/compare_nh.py 0.1 1000 ../outputs/compare_time_0_1_trials_1000/seed 100 > ../outputs/compare_time_0_1_trials_1000/log.log 2>&1 &
