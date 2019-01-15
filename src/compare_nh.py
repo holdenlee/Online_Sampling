619f44f64c1fe23db0f0396be9497e3b3dedd3a5
Holden Lee <holdenl@princeton.edu>
2019-01-15 16:31:21 -0500

    1-15

--- src/compare_nh.py
+++ src/compare_nh.py
@@ -43,18 +43,20 @@ verbosity=1
 
 batch_size = 64
 
-for seed in range(1,seeds+1):
+for seed in range(2,seeds+1):
     agents = make_default_agents(num_articles, dim, sparsity, time_limit, verbosity=verbosity, batch_size=batch_size, bias_term=True)
     results, cum_regrets = simple_compare(agents, num_articles, dim, sparsity, T, seed, verbosity=verbosity, dist_type='Bernoulli', slurm=True)
     print(results)
     print(cum_regrets)
     pickle.dump(results[0], open('%s_%d_results.p' % (name, seed),'wb'))
-    pickle.dump(regrets[0], open('%s_%d_regrets.p' % (name, seed),'wb'))
+    pickle.dump(cum_regrets[0], open('%s_%d_regrets.p' % (name, seed),'wb'))
     
 
 #run:
 #nohup python compare_nh.py 0.1 1000 ../outputs/compare_time_0_1_trials_1000/seed 100 > ../outputs/compare_time_0_1_trials_1000/log.log 2>&1 &
-
+"""
+[30.112570676021964, 55.61220382233826, 44.82029246480474, 34.71447869419428, 35.94746494602103, 100.33048996420474, 41.95934741021247, 33.96480808424565, 39.080878341643015]
+"""
 
 
 
